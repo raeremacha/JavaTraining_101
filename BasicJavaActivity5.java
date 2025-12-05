025bfb981ffdcb398fd40b42c650c13cddefb39c
Raisa Alexis E. Remacha <raeremacha@headoffice.corp.bpi.com.ph>
2025-12-05 11:15:59 +0800

    Java M1 Activity 5

---
+++ BasicJavaActivity5.java
@@ -0,0 +1,18 @@
+package BasicJavaActivity5;
+
+public class BasicJavaActivity5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       int sum = 0;
+
+	        // Loop from 1 to 50
+	        for (int i = 1; i <= 50; i++) {
+	            sum += i; // Add each number to sum
+	        }
+
+	        // Print the result
+	        System.out.println("Sum: " + sum);
+	}
+
+}
