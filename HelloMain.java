f1d4ada46f88683f595f58829968ce70134fd379
Raisa Alexis E. Remacha <raeremacha@headoffice.corp.bpi.com.ph>
2025-12-05 10:06:47 +0800

    Java Activity M1 Act1

---
+++ HelloMain.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+package com.bpi.hello;
+import java.util.Scanner;
+
+
+
+/**
+ * 
+ */
+public class HelloMain {
+    public static void main(String[] args) {
+
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.println("What is your name?");
+    	String name = sc.nextLine();
+    	
+    	System.out.println("Hello," + name + "!");
+    	
+    	
+    	
+    }
+}
