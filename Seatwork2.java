ba26e3e456d3782171c2be484a79d844f079f2bb
Raisa Alexis E. Remacha <raeremacha@headoffice.corp.bpi.com.ph>
2025-12-05 11:22:15 +0800

    Java M2 Activity 2

---
+++ Seatwork2.java
@@ -0,0 +1,11 @@
+
+package seatwork2;
+
+public class Seatwork2 {
+
+    public static void main(String[] args) {
+        Whiteboard wb = new Whiteboard(); 
+        int sum = wb.addNumbers(5, 3);    
+        System.out.println("The total is: " + sum);
+    }
+}
---
+++ Whiteboard.java
@@ -0,0 +1,9 @@
+package seatwork2;
+
+public class Whiteboard {
+
+	int addNumbers(int addend1, int addend2) {
+		int sum = addend1 + addend2;
+		return sum;
+	}
+}
